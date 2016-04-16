5dcfca48a074049cbd2e7917e66304c3fd3fd8a9
Col Harris <col.w.harris@gmail.com>
2016-04-17 03:30:39 +0800

    Add hex package details

--- mix.exs
+++ mix.exs
@@ -7,25 +7,23 @@ defmodule Telegram.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: "Simple module for parsing Telegram Bot API request",
+     package: package,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
+  def package do
+    [
+      maintainers: ["Colin Harris"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/col/telegram"}
+    ]
+  end
+
   def application do
     [applications: [:logger]]
   end
-
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
+  
   defp deps do
     [
       {:poison, "~> 2.0"}
